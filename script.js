d653a77120157587730e0ca1de5a4d58a7f3de9a
Eredeti <93719013+Eredeti@users.noreply.github.com>
2024-02-06 16:55:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.onload = function() {
-    var images = ["chemin/vers/gif1.gif", "chemin/vers/gif2.gif", "chemin/vers/gif3.gif"];
-    var texts = ["Texte 1", "Texte 2", "Texte 3"];
+    var images = ["premier.gif", "deuxième.gif"];
+    var texts = ["GIF 1", "GIF 2"];
     var index = 0;
     var gif = document.getElementById('gif');
     var textElement = document.getElementById('text');
