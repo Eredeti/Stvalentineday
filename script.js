e3f5c9622614bfdb41cf0d72cc749744afaac670
Eredeti <93719013+Eredeti@users.noreply.github.com>
2024-02-06 15:12:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -9,20 +9,17 @@ document.addEventListener('DOMContentLoaded', function() {
         responseDiv.textContent = 'Youpi!';
         responseDiv.style.fontSize = '24px';
         responseDiv.style.color = 'green';
-        secondGif.setAttribute('src', 'chemin/vers/ton/deuxieme/gif.gif');
+        secondGif.setAttribute('src', 'deuxième.gif');
     });
 
     let noBtnClicks = 0;
     noBtn.addEventListener('click', function() {
-        if (noBtnClicks === 0) {
+        if (noBtnClicks % 3 == 0) {
             noBtn.textContent = 'Tu es sûr ?';
-            responseDiv.textContent = 'Pas encore convaincu ?';
-        } else if (noBtnClicks === 1) {
+        } if (noBtnClicks % 3=== 1) {
             noBtn.textContent = 'Encore une fois !';
-            responseDiv.textContent = 'Vraiment ?';
-        } else {
+         }if (noBtnClicks % 3 === 2) {
             noBtn.textContent = 'Ok, ok...';
-            responseDiv.textContent = 'Peut-être la prochaine fois !';
             responseDiv.style.fontSize = '16px';
         }
         noBtnClicks++;
