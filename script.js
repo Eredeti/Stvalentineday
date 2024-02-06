e75a62b227301b0ddcaf1ed7d0d81aa131ea935f
Eredeti <93719013+Eredeti@users.noreply.github.com>
2024-02-06 15:28:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ document.addEventListener('DOMContentLoaded', function() {
         }
         noBtnClicks++;
 
-        yesBtn.style.fontSize = `${parseInt(yesBtn.style.fontSize || 16) + 5}px`;
+        yesBtn.style.fontSize = `${parseInt(yesBtn.style.fontSize || 16) + 25}px`;
 
         // Réinitialiser le deuxième GIF
         secondGif.setAttribute('src', '');
