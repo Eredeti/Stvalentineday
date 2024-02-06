4a14c4123027c6376eb74ed9955a2ab5f679ad8f
Eredeti <93719013+Eredeti@users.noreply.github.com>
2024-02-06 16:53:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 window.onload = function() {
-    var images = ["premier.gif", "deuxième.gif"];
+    var images = ["chemin/vers/gif1.gif", "chemin/vers/gif2.gif", "chemin/vers/gif3.gif"];
+    var texts = ["Texte 1", "Texte 2", "Texte 3"];
     var index = 0;
     var gif = document.getElementById('gif');
+    var textElement = document.getElementById('text');
 
     setInterval(function() {
         index = (index + 1) % images.length;
         gif.src = images[index];
+        textElement.textContent = texts[index];
     }, 5000);
 };
