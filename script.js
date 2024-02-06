97449bcb7bd8e6ec8eb733be41347a19e0880694
Eredeti <93719013+Eredeti@users.noreply.github.com>
2024-02-06 16:45:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,10 @@
-let changement=0;
-let compteur =0;
+window.onload = function() {
+    var images = ["premier.gif", "deuxième.gif"];
+    var index = 0;
+    var gif = document.getElementById('gif');
 
-const titre = document.getElementById('titre');
-const gif = document.getElementById('gif');
-const bouton1 = document.getElementById('bouton1');
-const bouton2 = document.getElementById('bouton2');
-
-document.querySelector('h1').textContent = "Coucou";
-gif.src = "premier.gif";
-
-if changement === 1{
-    titre.innerHTML = "YOUPIIIIII !";
-    gif.src = "deuxième.gif";
-    bouton1.style.display = "none";
-    bouton2.style.display = "none";
-}
-
-bouton1.addEventListener('click', function() {
-    changement=1;
-});
-
-bouton2.addEventListener('click', function() {
-    compteur = compteur + 1;
-    if (compteur % 3 === 0) bouton2.textContent = "Tu es sûr";
-    if (compteur % 3 === 1) bouton2.textContent = "Réfléchis bien";
-    if (compteur % 3 === 2) bouton2.textContent = "Réfléchis mieux !";
-});
+    setInterval(function() {
+        index = (index + 1) % images.length;
+        gif.src = images[index];
+    }, 5000);
+};
