4b5e66b0f463866da60afa9eca651bfe974f9326
Eredeti <93719013+Eredeti@users.noreply.github.com>
2024-02-06 15:23:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,34 +2,30 @@ document.addEventListener('DOMContentLoaded', function() {
     const yesBtn = document.getElementById('yes-btn');
     const noBtn = document.getElementById('no-btn');
     const responseDiv = document.getElementById('response');
-    const secondGifContainer = document.getElementById('second-gif-container');
+    const firstGif = document.getElementById('first-gif');
     const secondGif = document.getElementById('second-gif');
-    const oui = 0;
 
-    
     yesBtn.addEventListener('click', function() {
         responseDiv.textContent = 'Youpi!';
         responseDiv.style.fontSize = '24px';
         responseDiv.style.color = 'green';
-        secondGif.setAttribute('src', 'deuxième.gif');
+        firstGif.style.display = 'none'; // Cacher le premier GIF
+        secondGif.setAttribute('src', 'deuxième.gif'); // Afficher le deuxième GIF
     });
 
     let noBtnClicks = 0;
     noBtn.addEventListener('click', function() {
         if (noBtnClicks % 3 == 0) {
             noBtn.textContent = 'Tu es sûr ?';
-        } if (noBtnClicks % 3 == 1) {
+        } else if (noBtnClicks % 3 == 1) {
             noBtn.textContent = 'Réfléchis bien';
-         }if (noBtnClicks % 3 == 2) {
+        } else if (noBtnClicks % 3 == 2) {
             noBtn.textContent = 'Pardon ?';
             responseDiv.style.fontSize = '16px';
         }
         noBtnClicks++;
 
-        // Afficher le bouton "Oui" si ce n'est pas déjà le cas
-        if (yesBtn.style.display === 'none') {
-            yesBtn.style.display = 'inline-block';
-        }
+        yesBtn.style.fontSize = `${parseInt(yesBtn.style.fontSize || 16) + 1}px`;
 
         // Réinitialiser le deuxième GIF
         secondGif.setAttribute('src', '');
